356a7b20d507e987c3862ddb580d1d405fb6789e
zkj <438101845@qq.com>
2020-08-12 20:15:32 +0800

    [zkj]-{feat}-{exercise2}: add Products.js of data

--- src/exercise-1/components/products/Products.js
+++ src/exercise-1/components/products/Products.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-//import './aboutUs.css';
+import '../router/router.css';
 
 class Products extends React.Component {
 
+  state= {
+    products: [
+      {
+        "id": 1,
+        "name": "Bicycle",
+        "price": 30,
+        "quantity": 15,
+        "desc": "Bicycle is Good"
+      },
+      {
+        "id": 2,
+        "name": "TV",
+        "price": 40,
+        "quantity": 16,
+        "desc": "TV is good"
+      },
+      {
+        "id": 3,
+        "name": "Pencil",
+        "price": 50,
+        "quantity": 17,
+        "desc": "Pencil is good"
+      }
+    ]
+  }
+
+
 
   render() {
-    return <div className="content">
-      <p className="about-p">
-        <span>Compang: ThoughtWorks Local</span>
-      </p>
-      <p className="about-p">
-        <span>For more information, please</span>
-        <span>view our&nbsp;    
-          <Link to="/" className="website" >website.</Link>
-        </span>
-      </p>
-
+    return <div className="content flex-col">
+      <span>All Products</span>
+      {this.state.products.map(e => <span className="to-orange">{e.name}</span>)}
     </div>
   }
 }
