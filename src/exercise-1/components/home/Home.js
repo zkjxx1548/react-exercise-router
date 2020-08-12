2ab979ce79ae340bc5568b43f7824876debf6afd
zkj <438101845@qq.com>
2020-08-12 19:04:25 +0800

    [zkj]-{feat}: add .content to router.css

--- src/exercise-1/components/home/Home.js
+++ src/exercise-1/components/home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './home.css';
+import '../router/router.css';
 
 class Home extends React.Component {
   render() {
-    return <div className="home">
+    return <div className="content">
     <p>This is a beautiful Home Page.</p>
     <p className="url">And the url is '/'.</p>
   </div>
