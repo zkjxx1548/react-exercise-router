50869e3a9412f11dc888ba5660fc959408ee7da6
zkj <438101845@qq.com>
2020-08-12 19:36:08 +0800

    [zkj]-{feat}: the header has text-decoration when in the page

--- src/exercise-1/components/router/Router.js
+++ src/exercise-1/components/router/Router.js
@@ -6,15 +6,44 @@ import MyProfile from '../myProfile/MyProfile';
 import AboutUs from '../aboutUs/AboutUs';
 import './router.css'
 
-
 class Router extends React.Component {
+  state = {
+    textDecorationHome: '',
+    textDecorationMyProfile: '',
+    textDecorationAboutUs: ''
+  }
+
+  handleHome = () => {
+    this.setState({
+      textDecorationHome: 'none',
+      textDecorationMyProfile: '',
+      textDecorationAboutUs: ''
+    })
+  }
+
+  handleMyProfile = () => {
+    this.setState({
+      textDecorationHome: '',
+      textDecorationMyProfile: 'none',
+      textDecorationAboutUs: ''
+    })
+  }
+
+  handleAboutUs = () => {
+    this.setState({
+      textDecorationHome: '',
+      textDecorationMyProfile: '',
+      textDecorationAboutUs: 'none'
+    })
+  }
+
   render() {
     return (
       <BrowserRouter>
         <div className="navigation">
-          <Link to="/">Home</Link>
-          <Link to="/my-profile">MyProfile</Link>
-          <Link to="/about-us">AboutUs</Link>
+          <Link to="/" style={{textDecoration: this.state.textDecorationHome}} onClick={this.handleHome}>Home</Link>
+          <Link to="/my-profile" style={{textDecoration: this.state.textDecorationMyProfile}} onClick={this.handleMyProfile} >MyProfile</Link>
+          <Link to="/about-us" style={{textDecoration: this.state.textDecorationAboutUs}} onClick={this.handleAboutUs} >AboutUs</Link>
         </div>
         <Switch>
           <Route exact path="/" component={Home} />
