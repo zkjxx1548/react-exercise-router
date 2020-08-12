66d75edc6ce508e8428a99cd2c633f040425c849
zkj <438101845@qq.com>
2020-08-12 18:43:13 +0800

    [zkj]-{feat}: add AboutUs.js

--- src/exercise-1/components/AboutUs.js
+++ src/exercise-1/components/AboutUs.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class AboutUs extends React.Component {
   render() {
-    return <h1>da</h1>
+    return <div>
+      <p>
+        <span>Compang: ThoughtWorks Local</span>
+        <span>Location: Xi'an</span>
+      </p>
+      <p>
+        <span>For more information, please</span>
+        <span>view our </span>
+        <Link to="/">website.</Link>
+      </p>
+
+    </div>
   }
 }
 export default AboutUs;
